c583ec2279b927bfe6e70ad8f554f8c06eed184e
Joe Levin <jlevin@apppartner.com>
2016-06-17 14:33:30 -0400

    Initial Commit

---
+++ CarthagePractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarthagePractice
+//
+//  Created by App Partner on 6/17/16.
+//  Copyright © 2016 AppPartner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CarthagePractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarthagePractice
+//
+//  Created by App Partner on 6/17/16.
+//  Copyright © 2016 AppPartner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
